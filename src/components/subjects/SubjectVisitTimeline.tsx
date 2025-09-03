4869c3a3edae1c34fa109ce04bffe5951cbb51a5
bvquist1400 <b.bloomquist1@gmail.com>
2025-09-03 19:30:06 -0400

    fix(build): correct variable name in SubjectVisitTimeline map callback (_index instead of index)

--- src/components/subjects/SubjectVisitTimeline.tsx
+++ src/components/subjects/SubjectVisitTimeline.tsx
@@ -497,7 +497,7 @@ export default function SubjectVisitTimeline({
                       
                       <div className="flex items-center space-x-2">
                         <div className="text-right text-sm text-gray-400">
-                          #{index + 1}
+                          #{_index + 1}
                         </div>
                         <svg 
                           className={`w-5 h-5 text-gray-400 transition-transform ${isExpanded ? 'rotate-180' : ''}`} 
